9d654687ace120d460cf1c5d659e9061aff8e7a7
Dillion <nasond432@gmail.com>
2022-11-29 16:08:21 -0500

    merge clock changes into ball-collision branch

--- tests/testslowclock.sv
+++ tests/testslowclock.sv
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+`timescale 1ns/1ns
 module testslowclock();
 	reg fastclk;
 	reg reset;
@@ -6,17 +6,21 @@ module testslowclock();
 
 	slow_clock 
 	#(
-		.MAX_COUNT(614400)
+		.MAX_COUNT(20'd614400)
 	)
-	slwclk (fastclk, reset, clock);
+	slwclk (
+		.fastclock(fastclk), 
+		.reset(reset),
+		.slowclock(clock)
+	);
 
 	initial begin
 		fastclk = 0;
 		reset = 1;
-		#20 reset = 0;
+		#54 reset = 0; // 54 ns reset pulse
 	end
 
 	always begin
-		#5 fastclk = ~fastclk;
+		#54 fastclk = ~fastclk; // 54 ns = 18.63MHz (Pixel Clock) 
 	end
 endmodule
