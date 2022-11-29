f2bcee1c961862512c3176f0e3188d5badaf372f
Altbot69 <73755186+Altbot69@users.noreply.github.com>
2022-11-29 16:44:08 -0500

    example score.sv final product

--- modules/score.sv
+++ modules/score.sv
@@ -5,4 +5,54 @@ module score (
 	output reg game_over
 );
 
+
+
+
+
+/*
+//This is the minimal Hex display for the score. We could create a seperate module file and it include it at the top.
+//But for now I'll showcase it this way
+
+module BCD_Display_Left_Score(
+input [3:0] BCD,
+output [6:0] LED_Segment //This would be replaced with current_score and every increment would change the HEX display
+);
+
+assign LED_Segment[0] = ((BCD[0]&!BCD[1])&(!BCD[2]&!BCD[3]))|((!BCD[0]&!BCD[1])&(BCD[2]&!BCD[3]))|((BCD[0]&BCD[1])&(!BCD[2]&BCD[3]))|((BCD[0]&!BCD[1])&(BCD[2]&BCD[3]));
+
+assign LED_Segment[1] = (!BCD[1]*BCD[0]*!BCD[3]*BCD[2])+(!BCD[1]*!BCD[0]*BCD[3]*BCD[2])+(BCD[1]*BCD[0]*BCD[3])+(BCD[1]*!BCD[0]*BCD[2]);
+
+assign LED_Segment[2] = (BCD[1]*!BCD[0]*!BCD[3]*!BCD[2])+(BCD[3]*BCD[2]*!BCD[1]*!BCD[0])+(BCD[3]*BCD[2]*BCD[1]);
+
+assign LED_Segment[3] = (BCD[0]*BCD[1]*BCD[2])+(!BCD[0]*!BCD[1]*BCD[2]*!BCD[3])+(BCD[0]*!BCD[1]*!BCD[2]*!BCD[3])+(!BCD[0]*BCD[1]*!BCD[2]*BCD[3]);
+
+assign LED_Segment[4] = (BCD[3]*!BCD[2]*!BCD[1]*BCD[0])+(!BCD[3]*BCD[0])+(!BCD[3]*BCD[2]*!BCD[1]*!BCD[0]);
+
+assign LED_Segment[5] = (BCD[3]*BCD[2]*!BCD[1]*BCD[0])+(!BCD[3]*BCD[2]*BCD[1]*BCD[0])+(!BCD[3]*!BCD[2]*BCD[1]*!BCD[0])+(BCD[0]*!BCD[3]*!BCD[2]);
+
+assign LED_Segment[6] = (!BCD[3]*!BCD[2]*!BCD[1])+(!BCD[3]*BCD[2]*BCD[1]*BCD[0])+(BCD[3]*BCD[2]*!BCD[1]*!BCD[0]);
+endmodule
+
+module BCD_Display_Right_Score(
+input [3:0] BCD,
+output [6:0] LED_Segment //This would be replaced with current_score and every increment would change the HEX display 
+);
+
+assign LED_Segment[0] = ((BCD[0]&!BCD[1])&(!BCD[2]&!BCD[3]))|((!BCD[0]&!BCD[1])&(BCD[2]&!BCD[3]))|((BCD[0]&BCD[1])&(!BCD[2]&BCD[3]))|((BCD[0]&!BCD[1])&(BCD[2]&BCD[3]));
+
+assign LED_Segment[1] = (!BCD[1]*BCD[0]*!BCD[3]*BCD[2])+(!BCD[1]*!BCD[0]*BCD[3]*BCD[2])+(BCD[1]*BCD[0]*BCD[3])+(BCD[1]*!BCD[0]*BCD[2]);
+
+assign LED_Segment[2] = (BCD[1]*!BCD[0]*!BCD[3]*!BCD[2])+(BCD[3]*BCD[2]*!BCD[1]*!BCD[0])+(BCD[3]*BCD[2]*BCD[1]);
+
+assign LED_Segment[3] = (BCD[0]*BCD[1]*BCD[2])+(!BCD[0]*!BCD[1]*BCD[2]*!BCD[3])+(BCD[0]*!BCD[1]*!BCD[2]*!BCD[3])+(!BCD[0]*BCD[1]*!BCD[2]*BCD[3]);
+
+assign LED_Segment[4] = (BCD[3]*!BCD[2]*!BCD[1]*BCD[0])+(!BCD[3]*BCD[0])+(!BCD[3]*BCD[2]*!BCD[1]*!BCD[0]);
+
+assign LED_Segment[5] = (BCD[3]*BCD[2]*!BCD[1]*BCD[0])+(!BCD[3]*BCD[2]*BCD[1]*BCD[0])+(!BCD[3]*!BCD[2]*BCD[1]*!BCD[0])+(BCD[0]*!BCD[3]*!BCD[2]);
+
+assign LED_Segment[6] = (!BCD[3]*!BCD[2]*!BCD[1])+(!BCD[3]*BCD[2]*BCD[1]*BCD[0])+(BCD[3]*BCD[2]*!BCD[1]*!BCD[0]);
+endmodule
+
+*/
+
 endmodule
